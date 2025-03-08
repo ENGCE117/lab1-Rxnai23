7b09489efb3f686222f5fc958f7438f7afa0418e
Rxnai23 <129729905+Rxnai23@users.noreply.github.com>
2025-03-08 16:13:06 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+void GetMatrix( int **value, int *row, int *col ) {
+    printf( "How many row, col ? " ) ;
+    scanf( "%d %d", row, col ) ;
+    *value = new int[ *row * *col ] ;
+
+    for (int i = 0 ; i < *row ; i++ ) {
+
+        for (int j = 0; j < *col ; j++) {
+            //printf( "value[%d][%d] = ", i, j ) ;
+            scanf("%d", &(*value)[ i * *col + j ]) ;
+        }
+        
+    }
+    
+}
+void PrintMatrix( int **value, int row, int col ) {
+    printf( "Matrix (2x3): \n" ) ;
+    for (int i = 0 ; i < row ; i++ ) {
+
+        for ( int j = 0; j < col ; j++ ) {
+            printf("%d ", (*value)[ i * col + j ]) ;
+        }
+        printf( "\n" );
+        
+    }
+}
+
 int main() {
-    //YOUR CODE HERE
+    int *data, m = 3, n = 3 ;
+    data = new int[ m * n ] ;
+    GetMatrix( &data, &m, &n ) ;
+    PrintMatrix( &data, m, n ) ;
     return 0 ;
 }//end function
